d70ec20d6191ee9fc1fc1411efd667cfbbaf03d0
Расим <yamirasim@yandex.ru>
2021-01-27 00:51:52 +0300

    Расчет миль при перелете

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int stoimostBileta = 500_000;
+        int tarif = 2000;
+        int mili = stoimostBileta / tarif;
+        System.out.println(mili);
+    }
+
+
+}
